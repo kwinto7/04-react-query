e6163d91844f2dd7203b6863ca5d169c9a8619ad
Olexandr Brylov <kwinto7@gmail.com>
2025-09-20 17:11:42 +0300

    delete spare type from movie, update the queryClient name

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import App from './components/App/App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const quryClient = new QueryClient()
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={quryClient}>
+    <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
